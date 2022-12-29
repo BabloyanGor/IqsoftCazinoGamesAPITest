ee4f830db10b7a4b0caa4fca0549e427da198d90
Gor Babloyan <g.babloyan@iqsoft.am>
2022-12-29 13:46:45 +0400

    changes

---
+++ CasinoGamesAPITest/CasinoGamesAPITest/src/test/java/testCases/UpcomingMatchesEmptyOrLessThenFive.java
@@ -0,0 +1,66 @@
+package testCases;
+
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class UpcomingMatchesEmptyOrLessThenFive extends BaseTest {
+
+    public int getUpcomingMatchesCount() {
+        int count = 0;
+        try {
+            Unirest.setTimeouts(0, 0);
+            HttpResponse<String> response = Unirest.get("https://sportsbookwebsitewebapi.craftbet.com/website/getupcomingmatches?LanguageId=en&TimeZone=4&SportId=1&IncludeMarketTypeIds=1&IncludeMarketTypeIds=6021&IncludeMarketTypeIds=38")
+                    .header("content-type", "application/json; charset=utf-8")
+                    .header("origin", "https://sportsbookwebsite.craftbet.com")
+                    .asString();
+            JSONObject jsonObjectBody = new JSONObject(response.getBody());
+            Unirest.shutdown();
+
+            JSONObject jsonObjectResponseObject = new JSONObject(jsonObjectBody.toString());
+            JSONArray jsonArrayGames = jsonObjectResponseObject.getJSONArray("Ms");
+            count = jsonArrayGames.length();
+            return count;
+        }
+        catch (Exception e) {
+            try {
+                Unirest.setTimeouts(0, 0);
+                HttpResponse<String> response = Unirest.get("https://sportsbookwebsitewebapi.craftbet.com/website/getupcomingmatches?LanguageId=en&TimeZone=4&SportId=1&IncludeMarketTypeIds=1&IncludeMarketTypeIds=6021&IncludeMarketTypeIds=38")
+                        .header("content-type", "application/json; charset=utf-8")
+                        .header("origin", "https://sportsbookwebsite.craftbet.com")
+                        .asString();
+                JSONObject jsonObjectBody = new JSONObject(response.getBody());
+                Unirest.shutdown();
+
+                JSONObject jsonObjectResponseObject = new JSONObject(jsonObjectBody.toString());
+                JSONArray jsonArrayGames = jsonObjectResponseObject.getJSONArray("Ms");
+                count = jsonArrayGames.length();
+                return count;
+            }
+            catch (Exception k) {
+                System.out.println("Count Exception  " + e);
+                return count;
+            }
+        }
+    }
+
+
+    @Test
+    public void getDuplicateGames() {
+        if (getUpcomingMatchesCount()<5){
+            Assert.fail("Games are less then " + getUpcomingMatchesCount());
+        }
+        else{
+            Assert.assertTrue(true);
+        }
+    }
+
+
+}
