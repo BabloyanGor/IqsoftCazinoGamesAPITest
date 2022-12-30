7de9c5e92fa804f1dc58512b111e614f28cfc611
GorBabloyan <104680741+GorBabloyan@users.noreply.github.com>
2022-12-31 00:48:46 +0400

    changes

--- CasinoGamesAPITest/CasinoGamesAPITest/src/test/java/testCases/UpcomingMatchesEmptyOrLessThenFive.java
+++ CasinoGamesAPITest/CasinoGamesAPITest/src/test/java/testCases/UpcomingMatchesEmptyOrLessThenFive.java
@@ -1,20 +1,28 @@
 package testCases;
 
+import TestDataJson.UpcomingMatchesJson;
+import com.google.gson.Gson;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
-import com.mashape.unirest.http.exceptions.UnirestException;
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
-import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
-import java.io.IOException;
+import java.util.ArrayList;
 
 public class UpcomingMatchesEmptyOrLessThenFive  {
+    UpcomingMatchesJson upcomingMatchesJson = new UpcomingMatchesJson();
+    int upcomingMatchesCount = 0;
+    int responseCod = 0;
+    String description = null;
+
+    ArrayList<Object> upcomingMatches = new ArrayList<>();
+    boolean alarmOn = false;
 
     public int getUpcomingMatchesCount() {
-        int count = 0;
+
+        int count=0;
         try {
             Unirest.setTimeouts(0, 0);
             HttpResponse<String> response = Unirest.get("https://sportsbookwebsitewebapi.craftbet.com/website/getupcomingmatches?LanguageId=en&TimeZone=4&SportId=1&IncludeMarketTypeIds=1&IncludeMarketTypeIds=6021&IncludeMarketTypeIds=38")
@@ -54,13 +62,48 @@ public class UpcomingMatchesEmptyOrLessThenFive  {
 
     @Test
     public void getDuplicateGames() {
-        if (getUpcomingMatchesCount()<5){
-            Assert.fail("Games are less then " + getUpcomingMatchesCount());
+        SoftAssert softAssert = new SoftAssert();
+        upcomingMatchesCount = getUpcomingMatchesCount();
+
+        if (upcomingMatchesCount<5){
+            responseCod = 10;
+            description = "Upcoming Matches count is less then 5";
+            alarmOn = true;
+            softAssert.fail("Games are less then " + getUpcomingMatchesCount());
         }
         else{
-            Assert.assertTrue(true);
+            responseCod = 0;
+            description = "null";
+            alarmOn = false;
+            softAssert.assertTrue(true);
         }
+
+
+        upcomingMatchesJson.setUpcomingMatchesCount(upcomingMatchesCount);
+        upcomingMatchesJson.setResponseCode(responseCod);
+        upcomingMatchesJson.setDescription(description);
+        upcomingMatchesJson.setAlarmOn(alarmOn);
+
+
+
+        JSONObject jsonObjectMain = new JSONObject();     // Working version
+        JSONObject jsonObjectResponseObject = new JSONObject();
+        JSONArray jsonArray = new JSONArray();
+
+        JSONObject jsonResponseObject = new JSONObject();
+        jsonResponseObject.put("ResponseCod",upcomingMatchesJson.getResponseCode());
+        jsonResponseObject.put("Description",upcomingMatchesJson.getDescription());
+
+        jsonObjectResponseObject.put("UpcomingMatchesCount",upcomingMatchesJson.getUpcomingMatchesCount());
+        jsonResponseObject.put("ResponseObject",jsonObjectResponseObject);
+
+        jsonArray.put(jsonResponseObject);
+
+        jsonObjectMain.put("UpcomingMatches",jsonArray);
+        jsonObjectMain.put("AlarmOn",alarmOn);
+
+
+        System.out.println(jsonObjectMain);
+
     }
-
-
 }
